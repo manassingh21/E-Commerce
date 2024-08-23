f8156ce201aac03934c757464afee349c00c26a9
Manas Singh <manassingh21@iitk.ac.in>
2024-08-23 11:47:33 +0530

    full project

--- fontend/src/Components/Popular/Popular.jsx
+++ fontend/src/Components/Popular/Popular.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popular.css'
-import data_product from '../Assests/data'
 import { Item } from '../Item/Item'
 
 
 export const Popular = () => {
+
+  const[popularProducts,setPoppularProducts] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:4000/popularinwomen').then((response)=>response.json())
+    .then((data)=>setPoppularProducts(data));
+  },[])
+
+
   return (
     <div className='popular'>
         <h1>Popular In Women</h1>
         <hr />
         <div className="popular-item">
-            {data_product.map((item,i)=>{
+            {popularProducts.map((item,i)=>{
                 return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
         </div>
